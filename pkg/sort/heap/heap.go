2b9f27385869d5aa32b4940793b2c583a7ab07c9
王俊锋 <wang1137095129@foxmail.com>
2021-10-06 21:26:35 +0800

    新增堆排序的降序排列，现存问题：堆排序时对下标值为一的元素无法处理

--- pkg/sort/heap/heap.go
+++ pkg/sort/heap/heap.go
@@ -3,48 +3,87 @@ package heap
 import "algorithm/pkg/sort"
 
 type heap struct {
-	arr sort.Array
-	n   int
+	sort.Array
 }
 
 func New(arr []int) *heap {
-	return &heap{arr: arr, n: len(arr) + 1}
+	return &heap{arr}
 }
 
 func (h heap) SortDesc() {
-
+	l := len(h.Array) - 1
+	for k := l / 2; k > 0; k-- {
+		h.sinkDesc(k, l)
+	}
+	for l > 1 {
+		sort.Exchange(h.Array, 1, l)
+		l--
+		h.sinkDesc(1, l)
+	}
 }
 
-func (h heap) SortAsc() {
-	l := len(h.arr) - 1
+func (h *heap) SortAsc() {
+	l := len(h.Array) - 1
+	//对每个子堆进行下沉操作，最后得到的堆是一个优先队列的堆
 	for k := l / 2; k > 0; k-- {
 		h.sinkAsc(k, l)
 	}
+	//然后依次删除头节点（将头节点和最后一个节点进行交换，然后堆大小减一，并对对进行下沉操作）
 	for l > 1 {
-		sort.Exchange(h.arr, 1, l)
+		sort.Exchange(h.Array, 1, l)
 		l--
 		h.sinkAsc(1, l)
 	}
 }
 
 func (h heap) GetArr() []int {
-	return h.arr
+	return h.Array
 }
 
-//下沉
+//sinkAsc 下沉
 func (h heap) sinkAsc(i, n int) {
 	for 2*i <= n {
 		j := 2 * i
-		if sort.Less(h.arr, i, j) {
-			if j+1 <= n && !sort.Less(h.arr, j+1, j) {
-				sort.Exchange(h.arr, i, j+1)
+		//判断节点的值是否小于左子节点的值
+		if sort.Less(h.Array, i, j) {
+			//当节点的值小于左子节点的值，
+			//并且右子节点的值小于左子节点的值时，
+			//交换节点和左子节点的值，
+			//否则交换节点与右子节点的值
+			if j+1 <= n && !sort.Less(h.Array, j+1, j) {
+				sort.Exchange(h.Array, i, j+1)
 				i = j + 1
 			} else {
-				sort.Exchange(h.arr, i, j)
+				sort.Exchange(h.Array, i, j)
 				i = j
 			}
-		} else if j+1 <= n && sort.Less(h.arr, i, j+1) {
-			sort.Exchange(h.arr, i, j+1)
+		} else
+		//当节点的值大于左节点的值时，
+		//判断是否有右子节点的值，
+		//以及节点的值是否小于右子节点的值，
+		//如果小于右子节点的值则交换
+		if j+1 <= n && sort.Less(h.Array, i, j+1) {
+			sort.Exchange(h.Array, i, j+1)
+			i = j + 1
+		} else {
+			break
+		}
+	}
+}
+
+func (h heap) sinkDesc(i, n int) {
+	for i*2 <= n {
+		j := i * 2
+		if sort.More(h.Array, i, j) {
+			if j+1 <= n && sort.More(h.Array, j, j+1) {
+				sort.Exchange(h.Array, j+1, i)
+				i = j + 1
+			} else {
+				sort.Exchange(h.Array, j, i)
+				i = j
+			}
+		} else if j+1 <= n && sort.More(h.Array, i, j+1) {
+			sort.Exchange(h.Array, j+1, i)
 			i = j + 1
 		} else {
 			break
--- pkg/sort/heap/heap_test.go
+++ pkg/sort/heap/heap_test.go
@@ -11,5 +11,13 @@ func TestHeap_SortAsc(t *testing.T) {
 	i := New(arr)
 	i.SortAsc()
 	fmt.Println(i.GetArr())
-	fmt.Println(sort.IsAscSorted(i.arr))
+	fmt.Println(sort.IsAscSorted(i.Array))
+}
+
+func TestHeap_SortDesc(t *testing.T) {
+	arr := []int{5, 1, 15, 3, 4, 3, 6, 0, 2, 1}
+	i := New(arr)
+	i.SortDesc()
+	fmt.Println(i.GetArr())
+	fmt.Println(sort.IsDescSorted(i.Array))
 }
